20cffd88abc22693526d78a30aba797e235104b7
HGGVT666 <84181131+HGGVT666@users.noreply.github.com>
2026-01-20 18:29:10 +0700

    Create ZERO.sol

---
+++ contracts/ZERO.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract ZERO {
+    string public name = "ZERO";
+    string public symbol = "ZERO";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply = 1_000_000_000 * 10**18;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "low balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+}
+
